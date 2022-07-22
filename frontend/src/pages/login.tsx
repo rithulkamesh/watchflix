f26acf2e53b8595ddf62b48eda570de22486f74e
Rithul Kamesh <hi@rithul.dev>
2022-07-22 11:37:12 +0530

    Completed Login Form Mock

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -20,18 +20,36 @@ const Login: React.FC = () => {
             <div className="items-center justify-center">
               <div className="bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 rounded">
                 <div className="p-10">
-                <div id="Title" className="text-4xl font-bold mb-10">Sign In</div>
-                <div id="Form" className="">
-                  <form>
+                  <div id="Title" className="text-4xl font-bold mb-10">
+                    Sign In
+                  </div>
+                  <div id="Form" className=" flex items-center justify-center">
+                    <form className="flex-col flex justify-center items-center">
+                      <input
+                        type="email"
+                        placeholder="Enter your Email"
+                        className="bg-black bg-opacity-70 rounded-sm mx-5 my-5 mb-0.5 w-80 h-12 indent-3"
+                      />
+                      <input
+                        type="password"
+                        placeholder="Enter your password"
+                        className="bg-black bg-opacity-70 rounded-sm mx-5 my-5  mt-3 w-80 h-12 indent-3"
+                      />
 
-                  </form>
-                </div>
-                <p className="text-center text-slate-100 text-xs">
-                Final Project | Rithul Kamesh and Apoorva YK
-              </p>
+                      {/* submit button */}
+                      <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-80 rounded-sm mb-10"
+                      >
+                        Sign In
+                      </button>
+                    </form>
+                  </div>
+                  <p className="text-center text-slate-100 text-xs">
+                    Class 12 Project | Rithul Kamesh and Apoorva YK
+                  </p>
                 </div>
               </div>
-  
             </div>
           </div>
         </div>
