4bd6be2048bc33490faccff23495f9f6c2d7dfed
Rithul Kamesh <hi@rithul.dev>
2022-09-21 23:50:32 +0530

    Added User route to get user data

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React from "react";
 import { FaTv } from "react-icons/fa";
 import { FiLogOut, FiHome } from "react-icons/fi";
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,8 +5,10 @@ import "../styles/index.module.css";
 import SideBar from "../components/sidebar";
 const Home: React.FC = () => {
   const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState("Rithul");
   const router = useRouter();
   const validate = async () => {
+
     fetch("http://localhost:3001/auth/validate", {
       method: "POST",
       credentials: "include",
@@ -19,6 +21,18 @@ const Home: React.FC = () => {
           router.push("/login");
         }
       });
+
+    fetch("http://localhost:3001/auth/user", {
+      method: "GET",
+      credentials: "include",
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.status === 200) {
+        setUser(JSON.parse(data.user).name.split(" ")[0]);
+      }
+    })
+  
   };
 
   if (loading) {
@@ -30,7 +44,9 @@ const Home: React.FC = () => {
       <SideBar />
       <div className="b-cont w-screen h-screen bg-gray-700 ">
         <div className="ml-24 h-screen">
-            <div className="p-5">hello</div>
+          {/*  Welcome Message */}
+            <div className="pt-[3em] pl-[1em] text-[100px] font-black">Welcome, {user}!</div>
+            <div className="p-5 pl-[3em] text-[35px] font-light">How are you doing today?</div>
         </div>
       </div>
     </div>
