ef80ca657e9d099986682818282ad2064710b979
Rithul Kamesh <hi@rithul.dev>
2022-10-31 00:18:20 +0530

    even more refactors

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Loader from '../components/loading';
 import '../styles/index.module.css';
-import { SideBar, SideBarIcon } from '../components/sidebar';
 import MovieCard from '../components/movieCard';
 
-import { BsGear } from 'react-icons/bs';
 import HomeLayout from '../layouts/home';
+import { validateLogin } from '../utils/fetch';
 const Home: React.FC = () => {
 	const [loading, setLoading] = useState(true);
 	const [user, setUser] = useState('Rithul');
-	const [isAdmin, setIsAdmin] = useState(false);
 	const greetings = [
 		'How are you doing today?',
 		'Pick something to watch...',
@@ -19,18 +17,11 @@ const Home: React.FC = () => {
 	];
 	const router = useRouter();
 	const validate = async () => {
-		fetch('http://localhost:3001/auth/validate', {
-			method: 'POST',
-			credentials: 'include'
+		validateLogin(()=> {
+			setLoading(false);
+		}, () => {
+			router.push('/login');
 		})
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.status === 200) {
-					setLoading(false);
-				} else {
-					router.push('/login');
-				}
-			});
 
 		fetch('http://localhost:3001/auth/user', {
 			method: 'GET',
@@ -40,7 +31,6 @@ const Home: React.FC = () => {
 			.then((data) => {
 				if (data.status === 200) {
 					setUser(JSON.parse(data.user).name.split(' ')[0]);
-					setIsAdmin(JSON.parse(data.user).isAdmin);
 				}
 			});
 	};
